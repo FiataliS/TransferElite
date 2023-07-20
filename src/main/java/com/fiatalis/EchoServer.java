1b3fe0d4240b70d3ff959a6015718874cffb39dc
FiataliS <FiataliS@gmail.com>
2023-07-20 17:35:18 +0500

    init

--- src/main/java/com/fiatalis/EchoServer.java
+++ src/main/java/com/fiatalis/EchoServer.java
@@ -16,7 +16,7 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.concurrent.ConcurrentLinkedDeque;
 
 @Slf4j
-public class EchoServer extends Thread {
+public class EchoServer implements Runnable{
     @Override
     public void run() {
         EventLoopGroup auth = new NioEventLoopGroup(1);
